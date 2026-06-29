4c32212ee01eabafe4f575e3363ea8f0c64cf202
Stephan Kästner <stephan.kaestner@art2all.de>
2026-06-29 23:18:08 +0200

    implement Sprint 09.2 space grid

--- src/components/debug/SpaceGridDebug.jsx
+++ src/components/debug/SpaceGridDebug.jsx
@@ -1,37 +1,39 @@
-import { Line } from "@react-three/drei";
+import { useMemo } from "react";
+import * as THREE from "three";
 
 export default function SpaceGridDebug() {
+  const geometry = useMemo(() => {
+    const positions = new Float32Array([
+      -1.2, -1.0, 0.15,
+      -1.2, 1.0, 0.15,
+      0, -1.0, 0.15,
+      0, 1.0, 0.15,
+      1.2, -1.0, 0.15,
+      1.2, 1.0, 0.15,
+      -1.2, -1.0, 0.15,
+      1.2, -1.0, 0.15,
+      -1.2, -0.5, 0.15,
+      1.2, -0.5, 0.15,
+      -1.2, 0, 0.15,
+      1.2, 0, 0.15,
+      -1.2, 0.5, 0.15,
+      1.2, 0.5, 0.15,
+      -1.2, 1.0, 0.15,
+      1.2, 1.0, 0.15,
+    ]);
+
+    const bufferGeometry = new THREE.BufferGeometry();
+    bufferGeometry.setAttribute(
+      "position",
+      new THREE.BufferAttribute(positions, 3)
+    );
+
+    return bufferGeometry;
+  }, []);
+
   return (
-    <>
-      <Line
-        points={[
-          [-1.2, -1.0, 0.15],
-          [-1.2, 1.0, 0.15],
-        ]}
-        color="red"
-        lineWidth={4}
-        depthTest={false}
-      />
-
-      <Line
-        points={[
-          [0, -1.0, 0.15],
-          [0, 1.0, 0.15],
-        ]}
-        color="red"
-        lineWidth={4}
-        depthTest={false}
-      />
-
-      <Line
-        points={[
-          [1.2, -1.0, 0.15],
-          [1.2, 1.0, 0.15],
-        ]}
-        color="red"
-        lineWidth={4}
-        depthTest={false}
-      />
-    </>
+    <lineSegments geometry={geometry}>
+      <lineBasicMaterial color="red" linewidth={4} depthTest={false} />
+    </lineSegments>
   );
 }
